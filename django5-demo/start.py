8305d917a3fd064e656a434658672d85b570bbd1
John Matthew <john@compunique.com>
2025-06-05 04:34:30 -0700

    chainguard + waitress  improvements

--- django5-demo/start.py
+++ django5-demo/start.py
@@ -1,6 +1,9 @@
-from waitress import serve
+import logging
 
+from waitress import serve
 from core.wsgi import application
+
+logging.basicConfig(level=logging.DEBUG)
 
 serve(application, 
       listen="0.0.0.0:8080",
